297f8cba8cb4b8cdb204536d63fa030d2a9117c0
willy <willy8742891@gmail.com>
2024-12-13 22:05:17 +0800

    feat: init css layout.

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,9 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { envPlugin } from './libs/env'
 
 const app = createApp(App)
 
+app.use(envPlugin)
 app.use(router)
 
 app.mount('#app')
